a16a70522470d3b482f301cdff0c7adbd9f75e8b
BrendanRomanDev <roman.bp15@gmail.com>
2024-01-02 14:23:07 -0500

    Undercurl support

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,28 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-
+--
 -- Undercurl
-vim.cmd([[ let &t_Cs = "\e[4:3m"]])
-vim.cmd([[ let &t_Cs = "\e[4:0m"]])
+vim.api.nvim_set_var("t_Cs", "e[4:3m")
+vim.api.nvim_set_var("t_Ce", "e[4:0m")
+
+vim.api.nvim_set_hl(0, "SpellBad", {
+  sp = "red",
+  undercurl = true,
+  cterm = { underline = true },
+})
+vim.api.nvim_set_hl(0, "SpellCap", {
+  sp = "yellow",
+  undercurl = true,
+  cterm = { underline = true },
+})
+vim.api.nvim_set_hl(0, "SpellRare", {
+  sp = "blue",
+  undercurl = true,
+  cterm = { underline = true },
+})
+vim.api.nvim_set_hl(0, "SpellLocal", {
+  sp = "orange",
+  undercurl = true,
+  cterm = { underline = true },
+})
+-- vim.opt.spell = true
