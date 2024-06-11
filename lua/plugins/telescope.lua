985b76c33343c1e833201777635a37c112591a0d
BrendanRomanDev <brendan.roman@thriveworks.com>
2024-06-11 14:53:49 -0400

    Telescope Update to '.pick'

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,8 +2,8 @@ local Util = require("lazyvim.util")
 return {
   "nvim-telescope/telescope.nvim",
   keys = {
-    { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
-    { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+    { "<leader>fF", Util.pick("files"), desc = "Find Files (root dir)" },
+    { "<leader>ff", Util.pick("files", { cwd = false }), desc = "Find Files (cwd)" },
   },
   opts = {
     defaults = {
