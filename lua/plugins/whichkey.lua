d10c665dd13a87f28e49348eae0f6c408b27c8bf
BrendanRomanDev <roman.bp15@gmail.com>
2025-01-14 18:22:40 -0500

    WhichkeyUpdate

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -6,7 +6,7 @@ return {
     --     z = false,
     --   },
     -- },
-    defaults = {
+    spec = {
       ["<leader>l"] = { name = "+lazy/+liveserver", u = "Live Server Up", d = "Live Server Down" },
       ["<leader>z"] = { name = "+telekasten", n = "new note", t = "tag search" },
       -- which-key needs an initial  to set the leader prefix, then others will still work
